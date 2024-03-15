e36d2e3e5ebfa1f207f1766790667db17ed4aa02
esrnru <41890986+esrnru@users.noreply.github.com>
2024-03-15 16:35:08 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 git clone https://github.com/esrnru/devopsproject.git
 cd devopsproject
