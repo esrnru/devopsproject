f12146124cee533410667fe5788e2584b24dbde3
root <root@GECE>
2024-03-15 15:04:37 +0300

    setup.sh modified

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 git clone https://github.com/esrnru/devopsproject.git
 cd devopsproject
 sudo docker compose up -d --build
